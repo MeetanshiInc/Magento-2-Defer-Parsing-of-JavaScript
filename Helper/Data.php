65422000c49f0b3888cf4e29740f26785027e381
JayParmarMT <jay.parmar@meetanshi.com>
2026-04-23 20:37:29 +0530

    Compatible with php 8.3-8.4 and Magento version 2.4.8-p4 - 1.0.1

--- Helper/Data.php
+++ Helper/Data.php
@@ -8,12 +8,22 @@ use Magento\Store\Model\ScopeInterface;
 
 class Data extends AbstractHelper
 {
+    /**
+     * Data constructor.
+     *
+     * @param Context $context
+     */
     public function __construct(Context $context)
     {
         $this->scopeConfig = $context->getScopeConfig();
         parent::__construct($context);
     }
 
+    /**
+     * Check if module is enabled.
+     *
+     * @return bool
+     */
     public function isEnabled()
     {
         $active = $this->scopeConfig->getValue('deferjs/general/active', ScopeInterface::SCOPE_STORE);
--- Model/Observer.php
+++ Model/Observer.php
@@ -5,33 +5,49 @@ namespace Meetanshi\DeferJS\Model;
 use Magento\Framework\Event\ObserverInterface;
 use Meetanshi\DeferJS\Helper\Data;
 
-
 class Observer implements ObserverInterface
 {
+    /**
+     * @var Data
+     *
+     * @SuppressWarnings(PHPMD.CamelCasePropertyName)
+     */
     protected $_helper;
 
+    /**
+     * Observer constructor.
+     *
+     * @param Data $helper
+     */
     public function __construct(
         Data $helper
-    )
-    {
+    ) {
         $this->_helper = $helper;
     }
 
+    /**
+     * Execute observer.
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     */
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        if (!$this->_helper->isEnabled())
+        if (!$this->_helper->isEnabled()) {
             return;
+        }
         $response = $observer->getEvent()->getData('response');
-        if (!$response)
+        if (!$response) {
             return;
+        }
         $html = $response->getBody();
-        if ($html == '')
+        if ($html == '') {
             return;
+        }
         $conditionalJsPattern = '@(?:<script type="text/javascript"|<script)(.*)</script>@msU';
-        preg_match_all($conditionalJsPattern, $html, $_matches);
-        $_js_if = implode('', $_matches[0]);
+        preg_match_all($conditionalJsPattern, $html, $matches);
+        $jsIf = implode('', $matches[0]);
         $html = preg_replace($conditionalJsPattern, '', $html);
-        $html .= $_js_if;
+        $html .= $jsIf;
         $response->setBody($html);
     }
 }
